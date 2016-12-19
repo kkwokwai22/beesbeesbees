95ee7f7eb37aeb9aa5687cba4e3b59ee93218e78
kkwokwai22 <kkwokwai22@gmail.com>
2016-12-19 14:53:17 -0800

    Finish exercies

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Grub.call(this);
+  Bee.call(this);
+  this.honeyPot = 0;
+  this.age = 10;
+  this.job = 'make honey';
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+}
 
